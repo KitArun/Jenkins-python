9c0337d73d274a228d66db46f23e1b2e01d4e543
KITTINAT <143639731+KitArun@users.noreply.github.com>
2026-03-09 13:26:05 +0700

    1

--- test_main.py
+++ test_main.py
@@ -5,7 +5,6 @@ from main import greeting
 class TestMain(unittest.TestCase):
     def test_greeting(self):
         self.assertEqual(greeting("User"), "Hello, User! Welcome to Jenkins CI/CD.")
-        
 
 if __name__ == "__main__":
     unittest.main()
