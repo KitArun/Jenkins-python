2d2195eb580740dac78862297a2ce68ed9b78c82
KITTINAT <143639731+KitArun@users.noreply.github.com>
2026-03-09 12:46:21 +0700

    update

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
-print("Hello Jenkins, I am Python!")
-print("Hello Jenkins, I am Python2")
+# main.py
+def greeting(name):
+    return f"Hello, {name}! Welcome to Jenkins CI/CD."
+
+if __name__ == "__main__":
+    message = greeting("My Friend")
+    print(message)
+    print("Execution Successful!")
---
+++ test_main.py
@@ -0,0 +1,10 @@
+# test_main.py
+import unittest
+from main import greeting
+
+class TestMain(unittest.TestCase):
+    def test_greeting(self):
+        self.assertEqual(greeting("User"), "Hello, User! Welcome to Jenkins CI/CD.")
+
+if __name__ == "__main__":
+    unittest.main()
